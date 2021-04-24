fbf1e86495aa8b1767f71f46ee1ef4e154b7d2d8
wuxudongxd <wuxudongxd@gmail.com>
2021-04-24 23:19:24 +0800

    :recycle: 增加解法：05.替换空格

--- 剑指offer/05. 替换空格/demo.ts
+++ 剑指offer/05. 替换空格/demo.ts
@@ -1,8 +1,44 @@
+/**
+ * 正则版
+ */
+// function replaceSpace(s: string): string {
+//     return s.replace(/ /g, "%20");
+// }
+
+
+/**
+ * 参考《剑指offer》，采用从后向前移动的思路
+ */
 function replaceSpace(s: string): string {
-    
-    // 正则版
-    return s.replace(/ /g, "%20");
-    
+  const originalLength = s.length;
+  let numberOfBlank = 0;
+  for (const ch of s) {
+      if (ch === " "){
+        numberOfBlank++;
+      }
+  }
+  if (numberOfBlank === 0) {
+      return s;
+  }
+  const newLength = originalLength + numberOfBlank * 2;
+  let indexOfOriginal = originalLength;
+  let indexOfNew = newLength;
+  let arrS: string[] = Array.from(s);
+  while(indexOfOriginal >=  0 && indexOfNew > indexOfOriginal){
+      if (arrS[indexOfOriginal] === " ") {
+        arrS[indexOfNew--] = "0";
+        arrS[indexOfNew--] = "2";
+        arrS[indexOfNew--] = "%";
+      }
+      else {
+        arrS[indexOfNew--] = arrS[indexOfOriginal];
+      }
+      indexOfOriginal--;
+  }
+  return arrS.join("")
 }
 
+/**
+ * 测试部分
+ */
 replaceSpace("We are happy.");
