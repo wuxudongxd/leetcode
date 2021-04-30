db86673e10a41b257527a5148b7b0dc6026ba87e
wuxudongxd <wuxudongxd@gmail.com>
2021-04-30 23:03:25 +0800

    feat(数据结构): 新增题目：92. 反转链表 II
    题目链接：https://leetcode-cn.com/problems/reverse-linked-list-ii/

---
+++ 数据结构/链表/92. 反转链表 II/demo.ts
@@ -0,0 +1,34 @@
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+let successor: ListNode | null = new ListNode();
+
+export function reverseN(head: ListNode, n: number): ListNode {
+  if (n === 1) {
+    successor = head.next;
+    return head;
+  }
+  let last: ListNode = reverseN(head.next as ListNode, n - 1);
+  (head.next as ListNode).next = head;
+  head.next = successor;
+  return last;
+}
+
+export function reverseBetween(
+  head: ListNode | null,
+  left: number,
+  right: number
+): ListNode | null {
+  if (left === 1) {
+    return reverseN(head as ListNode, right);
+  }
+  (head as ListNode).next = reverseBetween((head as ListNode).next as ListNode, left - 1, right - 1);
+  return head;
+}
+
