b6dbd4fe7ddbaac66f3f5ae96435df67d9192205
wuxudongxd <wuxudongxd@gmail.com>
2021-05-01 21:52:49 +0800

    feat(数据结构/二叉树): 新增题目：116. 填充每个节点的下一个右侧节点指针
    题目链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/

---
+++ 数据结构/二叉树/116. 填充每个节点的下一个右侧节点指针/demo.ts
@@ -0,0 +1,30 @@
+class Node {
+  val: number;
+  left: Node | null;
+  right: Node | null;
+  next: Node | null;
+  constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+export function connect(root: Node | null): Node | null {
+  if (root == null) {
+    return null;
+  }
+  connectTwoNode(root.left, root.right);
+  return root;
+}
+
+export function connectTwoNode(node1: Node | null, node2: Node | null): void {
+  if (node1 == null || node2 == null) {
+    return;
+  }
+  node1.next = node2;
+  connectTwoNode(node1.left, node1.right);
+  connectTwoNode(node1.right, node2.left);
+  connectTwoNode(node2.left, node2.right);
+}
