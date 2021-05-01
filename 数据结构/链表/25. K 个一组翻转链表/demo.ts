d5363131ef16d0dd4d7c6e16efaf53ce5a38679d
wuxudongxd <wuxudongxd@gmail.com>
2021-05-01 15:57:21 +0800

    feat(数据结构/链表): 新增题目：25. K 个一组翻转链表
    题目链接：https://leetcode-cn.com/problems/reverse-nodes-in-k-group/

---
+++ 数据结构/链表/25. K 个一组翻转链表/demo.ts
@@ -0,0 +1,62 @@
+import { ListNode, createNode, printNode } from "../../../utils/linkedList";
+/**
+ * 分组遍历节点
+ */
+export function reverseKGroup(
+  head: ListNode | null,
+  k: number
+): ListNode | null {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  const dummy: ListNode = new ListNode();
+  dummy.next = head;
+  let pre: ListNode = dummy;
+  let end: ListNode | null = dummy;
+
+  while ((end as ListNode).next !== null) {
+    for (let i = 0; i < k && end !== null; i++) {
+      end = end.next;
+    }
+    if (end === null) {
+      break;
+    }
+    let next: ListNode | null = end.next;
+    end.next = null;
+    let start: ListNode = pre.next as ListNode;
+    pre.next = reverse(start);
+    start.next = next;
+    pre = start;
+    end = start;
+  }
+  return dummy.next;
+}
+
+export function reverse(head: ListNode) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  let preNode: ListNode | null = null;
+  let curNode: ListNode | null = head;
+  let nextNode: ListNode | null = null;
+
+  while (curNode !== null) {
+    nextNode = curNode.next; //nextNode 指向下一个节点,保存当前节点后面的链表。
+    curNode.next = preNode; //将当前节点next域指向前一个节点   null<-1<-2<-3<-4
+    preNode = curNode; //preNode 指针向后移动。preNode指向当前节点。
+    curNode = nextNode; //curNode指针向后移动。下一个节点变成当前节点
+  }
+  return preNode;
+}
+
+
+
+/**
+ * 测试部分
+ */
+let arr: number[] = [1, 2, 3, 4, 5],
+  k: number = 3;
+
+let head: ListNode | null = createNode(arr);
+head = reverseKGroup(head, k);
+printNode(head);
