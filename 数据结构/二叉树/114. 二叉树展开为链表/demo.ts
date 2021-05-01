070186a9a5f7e551b2ac9c9d2615dc27b73eed85
wuxudongxd <wuxudongxd@gmail.com>
2021-05-01 23:54:04 +0800

    feat(数据结构/二叉树): 114. 二叉树展开为链表
    题目链接：https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/

---
+++ 数据结构/二叉树/114. 二叉树展开为链表/demo.ts
@@ -0,0 +1,31 @@
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+/**
+ Do not return anything, modify root in-place instead.
+ */
+function flatten(root: TreeNode | null): void {
+    if (root === null) return;
+    flatten(root.left)
+    flatten(root.right)
+
+    let left: TreeNode | null = root.left;
+    let right: TreeNode | null = root.right;
+
+    root.left = null;
+    root.right = left;
+
+    let p: TreeNode | null = root;
+    while(p.right !== null){
+        p = p.right;
+    }
+    p.right = right
+}
+
