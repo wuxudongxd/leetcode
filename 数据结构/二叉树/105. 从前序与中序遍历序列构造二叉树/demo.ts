88bce77dd31535b7d5b7ef5fa6c81cc703866b27
wuxudongxd <wuxudongxd@gmail.com>
2021-05-02 17:09:23 +0800

    feat(数据结构/二叉树): 新增题目：105. 从前序与中序遍历序列构造二叉树
    题目链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/

---
+++ 数据结构/二叉树/105. 从前序与中序遍历序列构造二叉树/demo.ts
@@ -0,0 +1,33 @@
+export class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+export function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+    return build(preorder, 0, preorder.length -1, inorder, 0, inorder.length -1)
+}
+
+export function build(preorder: number[], preStart: number, preEnd: number, inorder: number[], inStart: number, inEnd: number): TreeNode | null{
+    if (preStart > preEnd){
+        return null;
+    }
+    const rootVal: number = preorder[preStart]
+    let index: number = 0;
+    for (let i = inStart; i <= inEnd; i++){
+        if (rootVal === inorder[i]){
+            index = i;
+            break;
+        }
+    }
+    let leftSize: number = index - inStart
+    const root: TreeNode = new TreeNode(rootVal);
+    root.left = build(preorder, preStart + 1, preStart + leftSize, inorder, inStart, index - 1)
+    root.right = build(preorder, preStart + leftSize + 1, preEnd,inorder, index + 1, inEnd)
+    return root;
+}
