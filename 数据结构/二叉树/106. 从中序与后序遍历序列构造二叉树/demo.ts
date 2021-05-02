0911b7419c6c9ecaf0ec2672a6499864707ef781
wuxudongxd <wuxudongxd@gmail.com>
2021-05-02 18:22:24 +0800

    feat(数据结构/二叉树): 新增题目：106. 从中序与后序遍历序列构造二叉树
    题目链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/

---
+++ 数据结构/二叉树/106. 从中序与后序遍历序列构造二叉树/demo.ts
@@ -0,0 +1,25 @@
+import { TreeNode } from "../../../utils/binaryTree"
+
+export function buildTree(inorder: number[], postorder: number[]): TreeNode | null {
+    return build(inorder, 0, inorder.length - 1, postorder, 0, postorder.length - 1)
+}
+
+
+export function build(inorder: number[], inStart: number, inEnd: number, postorder: number[], postStart: number, postEnd: number): TreeNode | null {
+    if (inStart > inEnd) {
+        return null;
+    }
+    const rootVal: number = postorder[postEnd]
+    let index: number = 0;
+    for (let i = inStart; i <= inEnd; i++) {
+        if (rootVal === inorder[i]) {
+            index = i;
+            break;
+        }
+    }
+    let leftSize: number = index - inStart
+    const root: TreeNode = new TreeNode(rootVal);
+    root.left = build(inorder, inStart, index - 1, postorder, postStart, postStart + leftSize - 1)
+    root.right = build(inorder, index + 1, inEnd, postorder, postStart + leftSize, postEnd - 1)
+    return root;
+}
