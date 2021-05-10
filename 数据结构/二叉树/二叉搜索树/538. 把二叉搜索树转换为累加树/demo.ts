249db4c64ae6e9f47b0088aad63a0aab49b6c49c
wuxudongxd <wuxudongxd@gmail.com>
2021-05-10 23:56:24 +0800

    feat(数据结构/二叉树)：538.把二叉搜索树转换为累加树

---
+++ 数据结构/二叉树/二叉搜索树/538. 把二叉搜索树转换为累加树/demo.ts
@@ -0,0 +1,20 @@
+import { TreeNode } from "../../../../utils/binaryTree"
+
+let sum = 0;
+function convertBST(root: TreeNode | null): TreeNode | null {
+    if (root === null) {
+        return null;
+    }
+    convertBST(root.right)
+    sum += root.val;
+    root.val = sum;
+    convertBST(root.left)
+    return root
+};
+
+/**
+ * 测试部分
+ */
+const head = new TreeNode(0)
+head.right = new TreeNode(1)
+console.log(convertBST(head));
