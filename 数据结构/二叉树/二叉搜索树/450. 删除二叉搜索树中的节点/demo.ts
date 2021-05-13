c0057661dc70e9a4ec02e42b39e6a6126a2011f9
wuxudongxd <wuxudongxd@gmail.com>
2021-05-13 15:56:10 +0800

    feat(数据结构/二叉搜索树)：450. 删除二叉搜索树中的节点

---
+++ 数据结构/二叉树/二叉搜索树/450. 删除二叉搜索树中的节点/demo.ts
@@ -0,0 +1,23 @@
+import { TreeNode } from "../../../../utils/binaryTree"
+
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+    if (root === null) return null;
+    if (root.val == key) {
+        if (root.left === null) return root.right
+        if (root.right === null) return root.left
+        let minNode: TreeNode = getMin(root.right)
+        root.val = minNode.val
+        root.right = deleteNode(root.right, minNode.val)
+    } else if (root.left !== null && root.val > key) {
+        root.left = deleteNode(root.left, key)
+    } else if (root.right !== null && root.val < key) {
+        root.right = deleteNode(root.right, key)
+    }
+    return root;
+
+    function getMin(node: TreeNode): TreeNode {
+        // BST 最左边的就是最小的
+        while (node.left !== null) node = node.left
+        return node;
+    }
+};
