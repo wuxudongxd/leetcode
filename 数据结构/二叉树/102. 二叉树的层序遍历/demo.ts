5b324421f61744c7ff54f401ad322bb12055796b
wuxudongxd <wuxudongxd@gmail.com>
2021-05-13 18:55:06 +0800

    feat(数据结构/二叉树)：新增题目：102. 二叉树的层序遍历

---
+++ 数据结构/二叉树/102. 二叉树的层序遍历/demo.ts
@@ -0,0 +1,26 @@
+import { TreeNode } from "../binaryTree"
+
+function levelOrder(root: TreeNode | null): number[][] {
+    let res: number[][] = []
+    let queue: TreeNode[] = []
+    if (root !== null) {
+        queue.unshift(root)
+        while (queue.length !== 0) {
+            let n = queue.length
+            let level: number[] = []
+            for (let i = 0; i < n; i++) {
+                let node: TreeNode = queue.pop() as TreeNode
+                level.push(node.val)
+                if (node.left !== null) {
+                    queue.unshift(node.left)
+                }
+                if (node.right !== null) {
+                    queue.unshift(node.right)
+                }
+            }
+            res.push(level)
+        }
+    }
+    return res
+
+};
