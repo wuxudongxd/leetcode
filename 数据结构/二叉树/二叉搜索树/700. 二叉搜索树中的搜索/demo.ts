0d6f73fc265878ebb9a084b4b5c06dd1d426ae24
wuxudongxd <wuxudongxd@gmail.com>
2021-05-13 21:40:31 +0800

    feat(数据结构/二叉搜索树)：新增题目：700.二叉搜索树中的搜索

---
+++ 数据结构/二叉树/二叉搜索树/700. 二叉搜索树中的搜索/demo.ts
@@ -0,0 +1,13 @@
+import { TreeNode } from "../../binaryTree"
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (root === null) return null;
+    if (root.val === val) return root;
+    if (root.val < val) {
+        return searchBST(root.right, val)
+    }
+    if (root.val > val) {
+        return searchBST(root.left, val)
+    }
+    return null
+};
