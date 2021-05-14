92b65af20426a8bb1310e72983615163b001ebef
wuxudongxd <wuxudongxd@gmail.com>
2021-05-14 17:07:28 +0800

    feat(数据结构/二叉树)：新增题目：95.不同的二叉搜索树

---
+++ 数据结构/二叉树/二叉搜索树/96. 不同的二叉搜索树/demo.ts
@@ -0,0 +1,24 @@
+function numTrees(n: number): number {
+    let memo: number[][] = new Array(n).fill(0).map(() => new Array(n).fill(0))
+    return count(1, n);
+
+    function count(low: number, high: number): number {
+        if (low >= high) return 1;
+        if (memo[low - 1][high - 1] !== 0) return memo[low - 1][high - 1]
+        let res: number = 0;
+        
+        for (let i = low; i <= high; i++) {
+            let left: number = count(low, i - 1)
+            let right: number = count(i + 1, high)
+            res += left * right;
+        }
+        memo[low][high] = res
+
+        return res
+    }
+};
+
+/**
+ * 测试部分
+ */
+numTrees(5);
