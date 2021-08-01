f3e2a8212c7b7e57f1ac19839d2da1d583a4a2d7
wuxudongxd <wuxudongxd@gmail.com>
2021-08-01 18:13:41 +0800

    feat:新增题目：83. 删除排序链表中的重复元素

---
+++ 数据结构/队列/83. 删除排序链表中的重复元素.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let p = head;
+  while (p && p.next) {
+    if (p.val === p.next.val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return head;
+}
