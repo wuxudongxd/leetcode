dee56c08005074ed4bbe036d946776d2d2b7c3d7
wuxudongxd <wuxudongxd@gmail.com>
2021-09-12 18:10:00 +0800

    feat: 完善解法: 102. 二叉树的层序遍历

--- 数据结构/二叉树/102. 二叉树的层序遍历.ts
+++ 数据结构/二叉树/102. 二叉树的层序遍历.ts
@@ -1,25 +1,19 @@
 import { TreeNode } from "./binaryTree";
 
-function levelOrder(root: TreeNode | null): number[][] {
-  let res: number[][] = [];
-  let queue: TreeNode[] = [];
-  if (root !== null) {
-    queue.unshift(root);
-    while (queue.length !== 0) {
-      let n = queue.length;
-      let level: number[] = [];
-      for (let i = 0; i < n; i++) {
-        let node: TreeNode = queue.pop() as TreeNode;
-        level.push(node.val);
-        if (node.left !== null) {
-          queue.unshift(node.left);
-        }
-        if (node.right !== null) {
-          queue.unshift(node.right);
-        }
-      }
-      res.push(level);
+export function levelOrder(root: TreeNode | null): number[][] {
+  if (!root) return [];
+  const res: number[][] = [];
+  const queue: TreeNode[] = [root];
+  while (queue.length !== 0) {
+    let n = queue.length;
+    const level: number[] = [];
+    for (let i = 0; i < n; i++) {
+      let node: TreeNode = queue.pop();
+      level.push(node.val);
+      if (node.left) queue.unshift(node.left);
+      if (node.right) queue.unshift(node.right);
     }
+    res.push(level);
   }
   return res;
 }
