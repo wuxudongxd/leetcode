10e9f198b1cbec7ca452720660427ba512f06a7e
wuxudongxd <wuxudongxd@gmail.com>
2021-09-12 17:10:11 +0800

    feat: 完善解法: 111. 二叉树的最小深度

--- 数据结构/二叉树/111. 二叉树的最小深度.ts
+++ 数据结构/二叉树/111. 二叉树的最小深度.ts
@@ -1,20 +1,30 @@
 import { TreeNode, createTreeNode } from "./binaryTree";
 
-export function minDepth(root: TreeNode | null): number {
-  if (!root) {
-    return 0;
-  }
-  if (!root.left && root.right) {
-    return minDepth(root.right) + 1;
-  }
-  if (root.left && !root.right) {
-    return minDepth(root.left) + 1;
-  }
-  return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
-}
+// export function minDepth(root: TreeNode | null): number {
+//   if (!root) {
+//     return 0;
+//   }
+//   if (!root.left && root.right) {
+//     return minDepth(root.right) + 1;
+//   }
+//   if (root.left && !root.right) {
+//     return minDepth(root.left) + 1;
+//   }
+//   return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+// }
 
-// test
-// const root = createTreeNode([2, null, 3, null, 4, null, 5, null, 6]);
-const root = createTreeNode([1, 2, 3, 4, 5, 6, 7, 8]);
-const level = minDepth(root);
-console.log(level);
+/**
+ * BFS
+ */
+export function minDepth(root: TreeNode | null): number {
+  if (!root) return 0;
+  const q:[TreeNode, number][] = [[root, 1]];
+  while(q.length){
+    const [node, level] = q.shift();
+    if (!node.left && !node.right){
+      return level
+    }
+    if (node.left) q.push([node.left, level + 1]);
+    if (node.right) q.push([node.right, level + 1]);
+  }
+}
