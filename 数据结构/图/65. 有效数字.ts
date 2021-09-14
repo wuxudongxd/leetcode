1a0e7242bd92d37e70d6a9b7ccc720d7fb1392db
wuxudongxd <wuxudongxd@gmail.com>
2021-09-14 19:03:31 +0800

    feat: 新增题目：65. 有效数字

--- 数据结构/图/65. 有效数字.ts
+++ 数据结构/图/65. 有效数字.ts
@@ -1,3 +1,29 @@
 function isNumber(s: string): boolean {
-    
+  const graph = {
+    0: { blank: 0, sign: 1, ".": 2, digit: 6 },
+    1: { ".": 2, digit: 6 },
+    2: { digit: 3 },
+    3: { digit: 3, e: 4 },
+    4: { digit: 5, sign: 7 },
+    5: { digit: 5 },
+    6: { ".": 3, e: 4, digit: 6 },
+    7: { digit: 5 },
+  };
+  let state = 0;
+  for (let char of s.trim()) {
+    if (char >= "0" && char <= "9") {
+      char = "digit";
+    } else if (char === " ") {
+      char = "blank";
+    } else if (["+", "-"].includes(char)) {
+      char = "sign";
+    } else if (["E", "e"].includes(char)) {
+      char = "e";
+    }
+    state = graph[state][char];
+    if (state === undefined) {
+      return false;
+    }
+  }
+  return [3, 5, 6].includes(state);
 }
