580bdad087ebc33c5d1ab185efca47fa82f71bb1
wuxudongxd <wuxudongxd@gmail.com>
2021-09-16 15:33:10 +0800

    feat: add two sort functions

---
+++ 算法/排序/bubbleSort.ts
@@ -0,0 +1,11 @@
+export function bubbleSort(arr: number[]): number[] {
+  const array = Array.from(arr);
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - i - 1; j++) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+  return array;
+}
---
+++ 算法/排序/selectionSort.ts
@@ -0,0 +1,18 @@
+export function selectionSort(arr: number[]): number[] {
+  const array = Array.from(arr);
+  for (let i = 0; i < array.length - 1; i++) {
+    let minIdex = i;
+    for (let j = i; j < array.length; j++) {
+      if (array[j] < array[minIdex]) {
+        minIdex = j;
+      }
+    }
+    if (minIdex !== i) {
+      [array[minIdex], array[i]] = [array[i], array[minIdex]];
+    }
+  }
+  return array;
+}
+
+// test
+console.log(selectionSort([5, 4, 3, 2, 1]));
