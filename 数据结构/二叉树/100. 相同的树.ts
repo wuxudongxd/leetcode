8b5c95f95809f1f179f8f4913c02e8e9215bfc27
wuxudongxd <wuxudongxd@gmail.com>
2021-09-17 15:28:29 +0800

    feat: 新增题目：100.相同的树

---
+++ 数据结构/二叉树/100. 相同的树.ts
@@ -0,0 +1,11 @@
+import { TreeNode } from "./binaryTree";
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+  if (p === null && q === null) return true;
+  if (p === null || q === null) return false;
+  return (
+    isSameTree(p?.left, q?.left) &&
+    isSameTree(p?.right, q?.right) &&
+    p.val === q.val
+  );
+}
