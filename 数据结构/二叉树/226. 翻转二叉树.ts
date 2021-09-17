6afbcaf270c26979980c637098ab2d1ed2c3b7e5
wuxudongxd <wuxudongxd@gmail.com>
2021-09-17 15:28:02 +0800

    feat: 226. 翻转二叉树 新增解法

--- 数据结构/二叉树/226. 翻转二叉树.ts
+++ 数据结构/二叉树/226. 翻转二叉树.ts
@@ -1,13 +1,18 @@
-import { TreeNode } from "./binaryTree"
+import { TreeNode } from "./binaryTree";
+
+// export function invertTree(root: TreeNode | null): TreeNode | null {
+//   if (root === null) {
+//     return null;
+//   }
+//   let tmp: TreeNode | null = root.left;
+//   root.left = root.right;
+//   root.right = tmp;
+//   invertTree(root.left);
+//   invertTree(root.right);
+//   return root;
+// }
 
 export function invertTree(root: TreeNode | null): TreeNode | null {
-  if (root === null) {
-    return null;
-  }
-  let tmp: TreeNode | null = root.left;
-  root.left = root.right;
-  root.right = tmp;
-  invertTree(root.left);
-  invertTree(root.right);
-  return root;
+  if (root === null) return null;
+  return new TreeNode(root.val, invertTree(root.right), invertTree(root.left));
 }
