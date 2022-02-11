8ae8179cbfab6ccd758df47623873c04f6298a79
wuxudongxd <wuxudongxd@gmail.com>
2022-02-11 21:32:35 +0800

    ✨ feat: 17. 电话号码的字母组合

---
+++ hot100/17. 电话号码的字母组合.ts
@@ -0,0 +1,23 @@
+/* eslint-disable no-param-reassign */
+function letterCombinations(digits: string): string[] {
+  const map = [...new Array(8).keys()].map((num) => {
+    const base = num * 3;
+    return String.fromCharCode(base + 97, base + 98, base + 99);
+  });
+  map[5] = 'pqrs';
+  map[6] = 'tuv';
+  map[7] = 'wxyz';
+
+  const arr = digits.split('').map((digit) => map[Number(digit) - 2]);
+
+  let res = arr.length ? arr[0].split('') : [];
+
+  for (let i = 1; i < arr.length; i++) {
+    res = res.map((str) => arr[i].split('').map((char) => str + char)).flat(1);
+  }
+  console.log(res);
+
+  return res;
+}
+
+letterCombinations('7');
